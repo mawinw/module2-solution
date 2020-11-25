e4b3d7fb6446e4785065f19add6371c995033df6
Mawinw <time-whiter@hotmail.com>
2020-11-25 16:39:50 +0700

    main page load bugfix

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -116,8 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      $dc.loadMenuItems(chosenCategoryShortName);
-      var homeHtmlToInsertIntoMainPage = '';
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 'randomCategoryShortName', "'" + chosenCategoryShortName + "'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
